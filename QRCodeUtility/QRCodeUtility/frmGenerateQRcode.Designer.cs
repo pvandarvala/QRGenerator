d7ef22706c731f6ee97e3e0683c3acd4e111e976
pvandarvala <paresh.vandarvala@gmail.com>
2024-07-18 00:22:31 +0530

    Initial Project commit

---
+++ QRCodeUtility/QRCodeUtility/frmGenerateQRcode.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QRCodeUtility
+{
+    partial class frmGenerateQRcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.lblLink = new System.Windows.Forms.Label();
+            this.pbQrCode = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(72, 48);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(112, 40);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLink.Location = new System.Drawing.Point(72, 8);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(608, 27);
+            this.txtLink.TabIndex = 1;
+            // 
+            // lblLink
+            // 
+            this.lblLink.AutoSize = true;
+            this.lblLink.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLink.Location = new System.Drawing.Point(16, 8);
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(37, 20);
+            this.lblLink.TabIndex = 2;
+            this.lblLink.Text = "Link";
+            // 
+            // pbQrCode
+            // 
+            this.pbQrCode.Location = new System.Drawing.Point(16, 8);
+            this.pbQrCode.Name = "pbQrCode";
+            this.pbQrCode.Size = new System.Drawing.Size(648, 424);
+            this.pbQrCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbQrCode.TabIndex = 3;
+            this.pbQrCode.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtLink);
+            this.panel1.Controls.Add(this.btnGenerate);
+            this.panel1.Controls.Add(this.lblLink);
+            this.panel1.Location = new System.Drawing.Point(24, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(688, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pbQrCode);
+            this.panel2.Location = new System.Drawing.Point(32, 128);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(680, 440);
+            this.panel2.TabIndex = 5;
+            // 
+            // frmGenerateQRcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 600);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmGenerateQRcode";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label lblLink;
+        private System.Windows.Forms.PictureBox pbQrCode;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ QRCodeUtility/QRCodeUtility/frmGenerateQRcode.cs
@@ -0,0 +1,54 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace QRCodeUtility
+{
+    public partial class frmGenerateQRcode : Form
+    {
+        public frmGenerateQRcode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // string data = "https://www.12jyotirlingas.com/"; // Data to encode in the QR code
+            string data =txtLink.Text.Trim();
+
+            int qrCodeSize = 20;
+
+            // Create a QRCodeGenerator
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+
+            // Create a QRCode
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            // Convert the QRCode to a bitmap
+            Bitmap qrCodeImage = qrCode.GetGraphic(qrCodeSize); // 20 is the pixel size of the QR code
+
+            // Save or display the QR code image as needed
+            qrCodeImage.Save("QRCode.png");
+
+            //pbQrCode.Size = new Size(200, 200); // Set the fixed size
+            //pbQrCode.SizeMode = PictureBoxSizeMode.Normal;
+            //pbQrCode.Image = Image.FromFile("QRCode.png");
+           
+
+            Console.WriteLine("QR code generated!");
+        }
+        // Overlay an image onto another image (used to add a logo to the QR code)
+        static Bitmap OverlayImage(Bitmap qrCode, Bitmap logo)
+        {
+            using (Graphics g = Graphics.FromImage(qrCode))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(logo, new Rectangle(qrCode.Width / 3, qrCode.Height / 3, qrCode.Width / 3, qrCode.Height / 3));
+            }
+            return qrCode;
+        }
+    }
+}
